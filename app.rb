660146de368e06d44708f51fa3a796bcee87992b
Steven Lawton <sjlawton@comcast.net>
2015-02-23 16:17:59 -0500

    new

--- app.rb
+++ app.rb
@@ -3,8 +3,14 @@ require 'holidapi'
 require 'date'
 class MyWebApp < Sinatra::Base
 	get '/' do 
-		@holidays = HolidApi.get(month:Time.now.month, year:Time.now.year)
-    @birthdays = HolidApi.get(month:'11',year:'1992')
-		erb:index
+    @month = (params['month'] && params['month'] != "") ? params['month'] : 11
+    @year = (params['year'] && params['year'] != "") ? params['year'] : 1992
+    @country = (params['country'] && params['country'] != "") ? params['country'] : 'us'
+	begin
+		@holidays = HolidApi.get(month:@month, year:@year, country:@country)
+	rescue HolidApi::BadRequest => Error
+    @holidays = 'Error, bad input'
+	end
+	erb:index
 	end
 end
