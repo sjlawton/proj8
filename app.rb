0af24fe75f4741a9e88b93301a3db61b60728f03
Steven Lawton <sjlawton@comcast.net>
2015-02-27 17:49:46 -0500

    fixes

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class MyWebApp < Sinatra::Base
     @month = (params['month'] && params['month'] != "") ? params['month'] : 11
     @year = (params['year'] && params['year'] != "") ? params['year'] : 1992
     @country = (params['country'] && params['country'] != "") ? params['country'] : 'us'
-	begin
-		@holidays = HolidApi.get(month:@month, year:@year, country:@country)
-	rescue HolidApi::BadRequest => Error
-    @holidays = 'Error, bad input'
-	end
+    begin
+		  @holidays = HolidApi.get(month:@month, year:@year, country:@country)
+	  rescue HolidApi::BadRequest => Error
+      @holidays = 'Error, bad input'
+	  end
 	erb:index
 	end
 end
